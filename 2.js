ff32524725f8ea331b0c9870efecfcdadc1c09f6
Furkan Cabbar <ffcabbar@gmail.com>
2019-11-15 02:00:40 +0300

    completed

---
+++ 2.js
@@ -0,0 +1,7 @@
+let numbers = [1, 4, 9];
+
+let triples = numbers.map((i) =>  {
+    return i * 3;
+});
+
+console.log(triples);
---
+++ 3.js
@@ -0,0 +1,7 @@
+let numbers = [12, 5, 20, 130, 44, 9, 240];
+
+let newArray = numbers.filter((i) => {
+    return i < 15;
+});
+
+console.log(newArray);
